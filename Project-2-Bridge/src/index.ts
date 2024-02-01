43f00f453d31562cd780d76fc73f566d4dbf9b78
Chua, Shu Ern <shuernchua@kapps.com.sg>
2024-02-01 15:36:21 +0800

    First Commit of Script and CSV

--- Project-2-Bridge/src/index.ts
+++ Project-2-Bridge/src/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable-next-line no-unused-vars */
+import { createObjectCsvWriter } from "csv-writer";
 import "dotenv/config";
 import { AlchemyProvider, ethers } from "ethers";
 import bridgeImplementationContractABI from "../src/bridgeImplementationContractABI.js";
@@ -7,6 +8,7 @@ import bridgeImplementationContractABI from "../src/bridgeImplementationContract
 // CA: "0x6f1D09Fed11115d65E1071CD2109eDb300D80A27"
 // This is the contract that holds the state (token balance, owner, etc)
 const bridgeProxyContractCA = "0x6f1D09Fed11115d65E1071CD2109eDb300D80A27";
+const erc20Abi = ["function balanceOf(address) view returns (uint256)"];
 // Implementation Contract
 // CA : "0x12F4B7020b4694F1971479157A3a77106018809B"
 // This is the contract that holds the logic, but no state
@@ -14,9 +16,25 @@ const bridgeProxyContractCA = "0x6f1D09Fed11115d65E1071CD2109eDb300D80A27";
 const alchemyApiKey = process.env.ALCHEMY_API_KEY;
 // mainnet
 const network = "homestead";
+const provider = new AlchemyProvider(network, alchemyApiKey);
+
+const csvWriter = createObjectCsvWriter({
+  path: "tokenList.csv",
+  header: [
+    { id: "address", title: "ADDRESS" },
+    { id: "name", title: "NAME" },
+    { id: "symbol", title: "SYMBOL" },
+    { id: "decimals", title: "DECIMALS" },
+    {
+      id: "balance",
+      title: "BALANCE",
+    },
+  ],
+});
 
 async function main() {
-  const provider = new AlchemyProvider(network, alchemyApiKey);
+  // Query Bridge
+
   const bridgeProxyContractInstance = new ethers.Contract(
     bridgeProxyContractCA,
     bridgeImplementationContractABI,
@@ -26,6 +44,40 @@ async function main() {
   const tokenList: TokenList =
     await bridgeProxyContractInstance.getBridgeTokenList();
   console.log("owner:", ownerAddress);
-  console.log("TokenList", tokenList);
+  //   console.log("TokenList", tokenList);
+  console.log("tokenList.length:", tokenList.length);
+  const csvRecords = [];
+  for (let i = 0; i < tokenList.length; i++) {
+    const tokenAddress = tokenList[i][0];
+    const tokenName = tokenList[i][1];
+    const symbol = tokenList[i][2];
+    const decimals = tokenList[i][3];
+    const tokenBalance = await getTokenBalanceOfProxyContract(tokenAddress);
+    const balance = ethers.formatUnits(tokenBalance, decimals);
+
+    csvRecords.push({
+      address: tokenAddress,
+      name: tokenName,
+      symbol: symbol,
+      decimals: decimals,
+      balance: balance,
+    });
+  }
+  csvWriter.writeRecords(csvRecords);
 }
+
+async function getTokenBalanceOfProxyContract(
+  tokenContractAddress: string
+): Promise<bigint> {
+  const tokenContractInstance = new ethers.Contract(
+    tokenContractAddress,
+    erc20Abi,
+    provider
+  );
+  const tokenBalance = await tokenContractInstance.balanceOf(
+    bridgeProxyContractCA
+  );
+  return tokenBalance;
+}
+
 main();
